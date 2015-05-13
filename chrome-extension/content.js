c726d878778b7c26b6ed9ffeb8dd50bc000f5d10
Kenneth Auchenberg <kenneth@auchenberg.dk>
2015-05-13 19:25:18 +0800

    Namespacing of console.logs

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,6 +1,6 @@
 (function () {
 
-  document.addEventListener('RemoteDebug.requestAccess', function (e) {
+  document.addEventListener('window.remoteDebug.requestAccess', function (e) {
     chrome.runtime.sendMessage({
       cmd: 'attach'
     })
@@ -13,7 +13,7 @@
       if (window.confirm('Do you want to allow ' + requester + ' to remote debug this tab?')) {
         
         var evt = document.createEvent("CustomEvent");
-        evt.initCustomEvent("RemoteDebug.requestAccess", true, true);
+        evt.initCustomEvent("window.remoteDebug.requestAccess", true, true);
         document.dispatchEvent(evt);
 
       }
