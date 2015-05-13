45768672ecc530cd4d294436c783af82ba9b0c6c
Kenneth Auchenberg <kenneth@auchenberg.dk>
2015-05-13 19:06:32 +0800

    Inject window.remoteDebug API into pages.

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,14 +1,26 @@
 (function () {
 
-  window.remotedebug = {}
-  window.remotedebug.requestAccess = function () {
-    if (window.confirm('Do you want to allow <name> to remote debug your tab?')) {
-      chrome.runtime.sendMessage({
-        cmd: 'attach'
-      })
-    }
-  }
+  document.addEventListener('RemoteDebug.requestAccess', function (e) {
+    chrome.runtime.sendMessage({
+      cmd: 'attach'
+    })
+  });
 
-  window.remotedebug.requestAccess()
+  var s = document.createElement('script');
+  s.textContent = '(' + function() {
+    window.remoteDebug = {}
+    window.remoteDebug.requestAccess = function (requester) {
+      if (window.confirm('Do you want to allow ' + requester + ' to remote debug this tab?')) {
+        
+        var evt = document.createEvent("CustomEvent");
+        evt.initCustomEvent("RemoteDebug.requestAccess", true, true);
+        document.dispatchEvent(evt);
+
+      }
+    }
+  } + ')();';
+
+  document.documentElement.appendChild(s);
+  s.parentNode.removeChild(s);  
 
 })()
