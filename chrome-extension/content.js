d8d280ec837b30ce6560db4483d6f527438fe85e
Kenneth Auchenberg <kenneth@auchenberg.dk>
2015-05-14 10:34:49 +0800

    Formatting.

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -4,23 +4,23 @@
     chrome.runtime.sendMessage({
       cmd: 'attach'
     })
-  });
+  })
 
-  var s = document.createElement('script');
-  s.textContent = '(' + function() {
+  var s = document.createElement('script')
+  s.textContent = '(' + function () {
     window.remoteDebug = {}
     window.remoteDebug.requestAccess = function (requester) {
       if (window.confirm('Do you want to allow ' + requester + ' to remote debug this tab?')) {
-        
-        var evt = document.createEvent("CustomEvent");
-        evt.initCustomEvent("window.remoteDebug.requestAccess", true, true);
-        document.dispatchEvent(evt);
+
+        var evt = document.createEvent('CustomEvent')
+        evt.initCustomEvent('window.remoteDebug.requestAccess', true, true)
+        document.dispatchEvent(evt)
 
       }
     }
-  } + ')();';
+  } + ')();'
 
-  document.documentElement.appendChild(s);
-  s.parentNode.removeChild(s);  
+  document.documentElement.appendChild(s)
+  s.parentNode.removeChild(s)
 
 })()
