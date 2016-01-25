6ab3cd3e9deb2f0094e9345a4ddc1f905073b68c
Kenneth Auchenberg <kenneth@auchenberg.dk>
2016-01-25 12:17:39 -0800

    Add logging to stats worker

--- stats.js
+++ stats.js
@@ -2,12 +2,20 @@ var request = require('request')
 var fs = require('fs')
 
 request('http://devtoolsremote.com/_stats', function(err, response, body) {
-	if (err) throw err
+	if (err) {
+		console.log('err', err)
+		throw err
+	}
 
 	var body = JSON.parse(body)
 	var line = '' + Date.now() + ',' + body.count.targets + ',' + body.count.sockets + '\n'
 
+	console.log('stats.line', line)
+
 	fs.appendFile('web/_stats.txt', line, encoding='utf8', function (err) {
-    if (err) throw err
+		if (err) {
+			console.log('err', err)
+			throw err
+		}
 	})
 })
